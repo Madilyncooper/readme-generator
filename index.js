ecbb9850043b9b26f3f10cd6169efd6354b32885
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-04-24 09:55:41 -0700

    Added questions for inquier prompt

--- index.js
+++ index.js
@@ -1,13 +1,68 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+inquirer 
+.prompt ([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give a description of your project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How can someone install this project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How can someone use this project?',
+    },
+    {
+        type: 'checkbox',
+        name: 'technologies',
+        message: 'What technologies did you use?',
+        choices: ['Javascript', 'CSS', 'Node.js', 'HTML'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can someone contribute to this project?',
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'What license do you have for the project?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    
+])
+.then((answers) => {
+    console.log(answers);
+});
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
+
